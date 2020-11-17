4153710712a3870606704296225f789434beea0e
Ralf Junghanns <ralf.junghanns@gmail.com>
2020-11-17 15:57:54 +0100

    Update API

--- docker/database/views.sql
+++ docker/database/views.sql
@@ -1,4 +1,30 @@
+-- BEGIN SENSOR PARAMETER VIEW --
+create view view_sensor_parameters(id, name, project, location, parameters) as
+WITH sensors AS (
+    SELECT s_1.id,
+           s_1.project,
+           s_1.name,
+           s_1.location,
+           p.name AS parameter_name
+    FROM public.sensors s_1
+             JOIN parameters p ON s_1.id = p.sensor_id
+    GROUP BY p.type, s_1.id, s_1.project, s_1.name, p.name
+    ORDER BY s_1.id
+)
+SELECT s.id,
+       s.name,
+       s.project,
+       s.location,
+       array_agg(s.parameter_name) AS parameters
+FROM sensors s
+GROUP BY s.id, s.name, s.project, s.location;
+
+alter table view_sensor_parameters
+    owner to inowas;
+-- END SENSOR PARAMETER VIEW --
+
 -- BEGIN RAW DATA VIEW --
+drop view if exists view_data_raw;
 create view view_data_raw(id, project, sensor_name, parameter_name, date_time, value) as
 SELECT s.id                                          AS sensor_id,
        s.project                                     AS project_name,
@@ -16,6 +42,7 @@ alter table view_data_raw
 -- END RAW DATA VIEW --
 
 -- BEGIN 6H DATA VIEW --
+drop materialized view if exists view_data_6h;
 create materialized view view_data_6h as
 WITH data AS (
     SELECT s.id                                  AS sensor_id,
@@ -45,6 +72,7 @@ alter materialized view view_data_6h owner to inowas;
 -- END 6H DATA VIEW --
 
 -- BEGIN 12H DATA VIEW --
+drop materialized view if exists view_data_12h;
 create materialized view view_data_12h as
 WITH data AS (
     SELECT s.id                                  AS sensor_id,
@@ -74,55 +102,91 @@ alter materialized view view_data_12h owner to inowas;
 -- END 12H DATA VIEW --
 
 -- BEGIN 1D DATA VIEW --
+drop materialized view if exists view_data_1d;
 create materialized view view_data_1d as
 WITH data AS (
-    SELECT s.id                                          AS sensor_id,
-           s.project                                     AS project_name,
-           s.name                                        AS sensor_name,
-           p.name                                        AS parameter_name,
-           to_timestamp(d."timestamp"::double precision) AS date_time,
-           d.value                                       AS value
+    SELECT s.id                                  AS sensor_id,
+           s.project                             AS project_name,
+           s.name                                AS sensor_name,
+           p.name                                AS parameter_name,
+           to_timestamp(floor(d."timestamp"::double precision / 86400::double precision) *
+                        86400::double precision) AS date_time,
+           d.value                               AS value
     FROM sensors s
              LEFT JOIN parameters p ON s.id = p.sensor_id
              LEFT JOIN datasets ds ON p.id = ds.parameter_id
              LEFT JOIN public.data d ON ds.id = d.dataset_id
-    GROUP BY d.value, s.project, s.name, p.name, to_timestamp(d."timestamp"::double precision), s.id
+    GROUP BY (to_timestamp(d."timestamp"::double precision)), s.id, s.project, s.name, s.location, p.id, p.type, p.name,
+             d."timestamp", (to_timestamp(d."timestamp"::double precision)::date), d.value
 )
-SELECT data.sensor_id                          AS sensor_id,
-       data.project_name                       AS project_name,
-       data.sensor_name                        AS sensor_name,
-       data.parameter_name                     AS parameter_name,
-       date_trunc('day'::text, data.date_time) AS date_time,
-       round(avg(data.value)::numeric, 4)      AS value
+SELECT data.sensor_id                     AS sensor_id,
+       data.project_name                  AS project_name,
+       data.sensor_name                   AS sensor_name,
+       data.parameter_name                AS parameter_name,
+       data.date_time                     AS date_time,
+       round(avg(data.value)::numeric, 4) AS value
 FROM data
-GROUP BY data.sensor_id, data.project_name, data.sensor_name, data.parameter_name, date_time;
+GROUP BY data.sensor_id, data.project_name, data.sensor_name, data.parameter_name, data.date_time;
 
 alter materialized view view_data_1d owner to inowas;
 -- END 1D DATA VIEW --
 
--- BEGIN 1W DATA VIEW --
-create materialized view view_data_1w as
+-- BEGIN 2D DATA VIEW --
+drop materialized view if exists view_data_2d;
+create materialized view view_data_2d as
 WITH data AS (
-    SELECT s.id                                          AS sensor_id,
-           s.project                                     AS project_name,
-           s.name                                        AS sensor_name,
-           p.name                                        AS parameter_name,
-           to_timestamp(d."timestamp"::double precision) AS date_time,
-           d.value                                       AS value
+    SELECT s.id                                   AS sensor_id,
+           s.project                              AS project_name,
+           s.name                                 AS sensor_name,
+           p.name                                 AS parameter_name,
+           to_timestamp(floor(d."timestamp"::double precision / 172800::double precision) *
+                        172800::double precision) AS date_time,
+           d.value                                AS value
     FROM sensors s
              LEFT JOIN parameters p ON s.id = p.sensor_id
              LEFT JOIN datasets ds ON p.id = ds.parameter_id
              LEFT JOIN public.data d ON ds.id = d.dataset_id
-    GROUP BY d.value, s.project, s.name, p.name, to_timestamp(d."timestamp"::double precision), s.id
+    GROUP BY (to_timestamp(d."timestamp"::double precision)), s.id, s.project, s.name, s.location, p.id, p.type, p.name,
+             d."timestamp", (to_timestamp(d."timestamp"::double precision)::date), d.value
 )
-SELECT data.sensor_id                           AS sensor_id,
-       data.project_name                        AS project_name,
-       data.sensor_name                         AS sensor_name,
-       data.parameter_name                      AS parameter_name,
-       date_trunc('week'::text, data.date_time) AS date_time,
-       round(avg(data.value)::numeric, 4)       AS value
+SELECT data.sensor_id                     AS sensor_id,
+       data.project_name                  AS project_name,
+       data.sensor_name                   AS sensor_name,
+       data.parameter_name                AS parameter_name,
+       data.date_time                     AS date_time,
+       round(avg(data.value)::numeric, 4) AS value
 FROM data
-GROUP BY data.sensor_id, data.project_name, data.sensor_name, data.parameter_name, date_time;
+GROUP BY data.sensor_id, data.project_name, data.sensor_name, data.parameter_name, data.date_time;
+
+alter materialized view view_data_2d owner to inowas;
+-- END 2D DATA VIEW --
+
+-- BEGIN 1W DATA VIEW --
+drop materialized view if exists view_data_1w;
+create materialized view view_data_1w as
+WITH data AS (
+    SELECT s.id                                   AS sensor_id,
+           s.project                              AS project_name,
+           s.name                                 AS sensor_name,
+           p.name                                 AS parameter_name,
+           to_timestamp(floor(d."timestamp"::double precision / 604800::double precision) *
+                        604800::double precision) AS date_time,
+           d.value                                AS value
+    FROM sensors s
+             LEFT JOIN parameters p ON s.id = p.sensor_id
+             LEFT JOIN datasets ds ON p.id = ds.parameter_id
+             LEFT JOIN public.data d ON ds.id = d.dataset_id
+    GROUP BY (to_timestamp(d."timestamp"::double precision)), s.id, s.project, s.name, s.location, p.id, p.type, p.name,
+             d."timestamp", (to_timestamp(d."timestamp"::double precision)::date), d.value
+)
+SELECT data.sensor_id                     AS sensor_id,
+       data.project_name                  AS project_name,
+       data.sensor_name                   AS sensor_name,
+       data.parameter_name                AS parameter_name,
+       data.date_time                     AS date_time,
+       round(avg(data.value)::numeric, 4) AS value
+FROM data
+GROUP BY data.sensor_id, data.project_name, data.sensor_name, data.parameter_name, data.date_time;
 
 alter materialized view view_data_1w owner to inowas;
 -- END 1W DATA VIEW --
--- src-py/app/app.py
+++ src-py/app/app.py
@@ -1,7 +1,9 @@
 from flask import abort, Flask, request, render_template, jsonify
 from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
 import os
+import time
 
 db = SQLAlchemy()
 
@@ -13,40 +15,30 @@ db.init_app(app)
 @app.route('/', methods=['GET'])
 @cross_origin()
 def upload_file():
-    result = db.engine.execute(
-        "select d from public.view_data_1w where sensor_name='{0}' and project_name = '{1}' and parameter_name = '{2}'".format(
-            'I-3', 'DEU1', 'ec'
-        )
-    )
-    rows = [dict(row) for row in result]
-    print(rows)
     if request.method == 'GET':
-        return render_template('upload.html')
+        return jsonify([])
 
 
 @app.route('/list', methods=['GET'])
+@app.route('/sensors', methods=['GET'])
 @cross_origin()
 def sensor_list():
-    result = db.engine.execute("select * from sensors;")
+    result = db.engine.execute("select * from view_sensor_parameters;")
     sensor_list = [dict(row) for row in result]
-
-    for sensor in sensor_list:
-        result = db.engine.execute(
-            "select p.type from sensors join parameters p on sensors.id = p.sensor_id where sensors.id::text =\'{0}\' group by p.type;".format(
-                str(sensor['id'])))
-        sensor['parameters'] = [dict(row)['type'] for row in result]
-
     return jsonify(sensor_list)
 
 
 @app.route('/sensors/project/<project>/sensor/<sensor>/parameter/<parameter>')
 def sensor_data(project, sensor, parameter):
-    valid_time_resolution_list = ['RAW', '6H', '12H', '1D', '1W']
-    time_resolution = request.args.get('timeResolution', '1D')
+    valid_time_resolution_list = ['RAW', '6H', '12H', '1D', '2D', '1W']
+    time_resolution = request.args.get('timeResolution', '1D').upper()
+
+    start = int(request.args.get('start', '0'))
+    end = int(request.args.get('end', str(int(time.time()))))
 
     # string not in the list
     if time_resolution not in valid_time_resolution_list:
-        abort(400, 'Invalid timeResolution {0} provided. Valid values are: '.format(
+        abort(400, 'Invalid timeResolution {0} provided. Valid values are: {1}'.format(
             time_resolution,
             ', '.join(valid_time_resolution_list)
         ))
@@ -59,47 +51,72 @@ def sensor_data(project, sensor, parameter):
     if count == 0:
         return jsonify([])
 
+    query = ''
     if time_resolution == 'RAW':
-        result = db.engine.execute(
-            "select * from public.view_data_raw where sensor_name='{0}' and project = '{1}' and parameter_name = '{2}'".format(
-                sensor, project, parameter
-            )
-        )
-        return jsonify([dict(row) for row in result])
+        query = "select date_time, value from view_data_raw " \
+                "where date_time > to_timestamp('{0}') " \
+                "and date_time < to_timestamp('{1}') " \
+                "and sensor_name='{2}' " \
+                "and project = '{3}' " \
+                "and parameter_name = '{4}'" \
+            .format(start, end, sensor, project, parameter)
 
     if time_resolution == '6H':
-        result = db.engine.execute(
-            "select * from public.materialized_view_data_6h where sensor_name='{0}' and project = '{1}' and parameter_name = '{2}'".format(
-                sensor, project, parameter
-            )
-        )
-        return jsonify([dict(row) for row in result])
+        query = "select date_time, value from view_data_6h " \
+                "where date_time > to_timestamp('{0}') " \
+                "and date_time < to_timestamp('{1}') " \
+                "and sensor_name='{2}' " \
+                "and project_name = '{3}' " \
+                "and parameter_name = '{4}'" \
+            .format(start, end, sensor, project, parameter)
 
     if time_resolution == '12H':
-        result = db.engine.execute(
-            "select * from public.materialized_view_data_12h where sensor_name='{0}' and project = '{1}' and parameter_name = '{2}'".format(
-                sensor, project, parameter
-            )
-        )
-        return jsonify([dict(row) for row in result])
+        query = "select date_time, value from view_data_12h " \
+                "where date_time > to_timestamp('{0}') " \
+                "and date_time < to_timestamp('{1}') " \
+                "and sensor_name='{2}' " \
+                "and project_name = '{3}' " \
+                "and parameter_name = '{4}'" \
+            .format(start, end, sensor, project, parameter)
 
     if time_resolution == '1D':
-        result = db.engine.execute(
-            "select * from public.materialized_view_data_1d where sensor_name='{0}' and project = '{1}' and parameter_name = '{2}'".format(
-                sensor, project, parameter
-            )
-        )
-        return jsonify([dict(row) for row in result])
+        query = "select date_time, value from view_data_1d " \
+                "where date_time > to_timestamp('{0}') " \
+                "and date_time < to_timestamp('{1}') " \
+                "and sensor_name='{2}' " \
+                "and project_name = '{3}' " \
+                "and parameter_name = '{4}'" \
+            .format(start, end, sensor, project, parameter)
+
+    if time_resolution == '2D':
+        query = "select date_time, value from view_data_2d " \
+                "where date_time > to_timestamp('{0}') " \
+                "and date_time < to_timestamp('{1}') " \
+                "and sensor_name='{2}' " \
+                "and project_name = '{3}' " \
+                "and parameter_name = '{4}'" \
+            .format(start, end, sensor, project, parameter)
 
     if time_resolution == '1W':
-        result = db.engine.execute(
-            "select * from public.materialized_view_data_1w where sensor_name='{0}' and project = '{1}' and parameter_name = '{2}'".format(
-                sensor, project, parameter
-            )
-        )
-        return jsonify([dict(row) for row in result])
+        query = "select date_time, value from view_data_1w " \
+                "where date_time > to_timestamp('{0}') " \
+                "and date_time < to_timestamp('{1}') " \
+                "and sensor_name='{2}' " \
+                "and project_name = '{3}' " \
+                "and parameter_name = '{4}'" \
+            .format(start, end, sensor, project, parameter)
 
-    return jsonify([])
+    df = pd.read_sql_query(query, db.engine)
+    if (df.empty):
+        return jsonify([])
+
+    df.set_index('date_time') \
+        .resample(time_resolution) \
+        .interpolate(method='time') \
+        .reset_index(level=0) \
+        .rename(columns={"date_time": "ts", "value": "val"})
+
+    return df.to_json(date_unit='s', orient='records')
 
 
 if __name__ == '__main__':
@@ -107,4 +124,5 @@ if __name__ == '__main__':
     app.config['SESSION_TYPE'] = 'filesystem'
     app.config['DEBUG'] = os.environ.get("DEBUG", default='false') == 'true'
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI", default='')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ.get("DEBUG", default='false') == 'true'
     app.run(debug=app.config['DEBUG'], host='0.0.0.0')
