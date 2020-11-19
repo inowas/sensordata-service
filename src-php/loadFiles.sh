90a4bad695df60db9a35b76158e011cf9e5d7b58
Ralf Junghanns <ralf.junghanns@gmail.com>
2020-11-19 11:14:40 +0100

    Update loadFiles.sh

--- src-php/loadFiles.sh
+++ src-php/loadFiles.sh
@@ -5,10 +5,16 @@ set -e
 SCRIPT_PATH=$(dirname $0)
 SCRIPT_PATH="$( (cd $SCRIPT_PATH && pwd))"
 
-rsync --remove-source-files -azv ssh-w011ec33@w011ec33.kasserver.com:/www/htdocs/w011ec33/uit-sensor-data/processed/ $SCRIPT_PATH/data/UIT/inbox
-docker-compose exec php bin/console app:load-uit-files
-docker-compose exec php bin/console doctrine:query:sql "REFRESH MATERIALIZED VIEW public.view_data_6h;"
-docker-compose exec php bin/console doctrine:query:sql "REFRESH MATERIALIZED VIEW public.view_data_12h;"
-docker-compose exec php bin/console doctrine:query:sql "REFRESH MATERIALIZED VIEW public.view_data_1d;"
-docker-compose exec php bin/console doctrine:query:sql "REFRESH MATERIALIZED VIEW public.view_data_2d;"
-docker-compose exec php bin/console doctrine:query:sql "REFRESH MATERIALIZED VIEW public.view_data_1w;"
+INBOX_PATH=$SCRIPT_PATH/data/UIT/inbox
+
+rsync --remove-source-files -azv ssh-w011ec33@w011ec33.kasserver.com:/www/htdocs/w011ec33/uit-sensor-data/processed/ $INBOX_PATH
+
+files=(INBOX_PATH/*)
+if [ ${#files[@]} -gt 0 ]; then
+  docker-compose exec php bin/console app:load-uit-files
+  docker-compose exec php bin/console doctrine:query:sql "REFRESH MATERIALIZED VIEW public.view_data_6h;"
+  docker-compose exec php bin/console doctrine:query:sql "REFRESH MATERIALIZED VIEW public.view_data_12h;"
+  docker-compose exec php bin/console doctrine:query:sql "REFRESH MATERIALIZED VIEW public.view_data_1d;"
+  docker-compose exec php bin/console doctrine:query:sql "REFRESH MATERIALIZED VIEW public.view_data_2d;"
+  docker-compose exec php bin/console doctrine:query:sql "REFRESH MATERIALIZED VIEW public.view_data_1w;"
+fi
