ebb384ec9261f7a7e39e39405c5aab77e3617302
Ralf Junghanns <ralf.junghanns@gmail.com>
2020-11-19 10:24:42 +0100

    Update loadFiles.sh

--- src-php/loadFiles.sh
+++ src-php/loadFiles.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+rsync --remove-source-files -azv ssh-w011ec33@w011ec33.kasserver.com:/www/htdocs/w011ec33/uit-sensor-data/processed/ ./data/UIT/inbox
 docker-compose exec php bin/console app:load-uit-files
 docker-compose exec php bin/console doctrine:query:sql "REFRESH MATERIALIZED VIEW public.view_data_6h;"
 docker-compose exec php bin/console doctrine:query:sql "REFRESH MATERIALIZED VIEW public.view_data_12h;"
