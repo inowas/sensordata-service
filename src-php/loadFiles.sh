a897f99e63d30a239aebad3a16cfd8778fc6867f
Ralf Junghanns <ralf.junghanns@gmail.com>
2020-11-19 11:28:05 +0100

    Update loadFiles.sh

--- src-php/loadFiles.sh
+++ src-php/loadFiles.sh
@@ -9,7 +9,8 @@ INBOX_PATH=$SCRIPT_PATH/data/UIT/inbox
 
 rsync --remove-source-files -azv ssh-w011ec33@w011ec33.kasserver.com:/www/htdocs/w011ec33/uit-sensor-data/processed/ $INBOX_PATH
 
-files=(INBOX_PATH/*)
+shopt -s nullglob dotglob
+files=("$INBOX_PATH"/*)
 if [ ${#files[@]} -gt 0 ]; then
   docker-compose exec php bin/console app:load-uit-files
   docker-compose exec php bin/console doctrine:query:sql "REFRESH MATERIALIZED VIEW public.view_data_6h;"
@@ -17,4 +18,6 @@ if [ ${#files[@]} -gt 0 ]; then
   docker-compose exec php bin/console doctrine:query:sql "REFRESH MATERIALIZED VIEW public.view_data_1d;"
   docker-compose exec php bin/console doctrine:query:sql "REFRESH MATERIALIZED VIEW public.view_data_2d;"
   docker-compose exec php bin/console doctrine:query:sql "REFRESH MATERIALIZED VIEW public.view_data_1w;"
+  rm -rf ${"$INBOX_PATH"/*:?}
+  rm -rf ${"$INBOX_PATH"/.*:?}
 fi
