7b76fd51895a4a9785bb20ef2d20a4f5dbfbf18d
Ralf Junghanns <ralf.junghanns@gmail.com>
2020-11-25 10:45:23 +0100

    Add RemoveSensorCommand.php

--- src-php/src/Command/RemoveSensorCommand.php
+++ src-php/src/Command/RemoveSensorCommand.php
@@ -7,7 +7,6 @@ namespace App\Command;
 use App\Entity\Sensor;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
-use Ramsey\Uuid\Uuid;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -28,19 +27,19 @@ class RemoveSensorCommand extends Command
     protected function configure(): void
     {
         $this->setDescription('Removes a sensor from DB.');
-        $this->addArgument('id', InputArgument::REQUIRED);
+        $this->addArgument('project', InputArgument::REQUIRED);
+        $this->addArgument('sensor', InputArgument::REQUIRED);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-
+        $projectName = $input->getArgument('project');
+        $sensorName = $input->getArgument('sensor');
         try {
-            $sensor = $this->em
-                ->getRepository(Sensor::class)
-                ->findOneBy(['id' => Uuid::fromString($input->getArgument('id'))]);
-
+            $sensor = $this->em->getRepository(Sensor::class)->findOneBy(['project' => $projectName, 'name' => $sensorName]);
             if ($sensor instanceof Sensor) {
                 $this->em->remove($sensor);
+                $this->em->flush();
                 return Command::SUCCESS;
             }
         } catch (Exception $exception) {
