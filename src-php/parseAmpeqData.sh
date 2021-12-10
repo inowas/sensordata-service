220c9f4e5974dc3223b4a0059bb8cc4449977ddf
Ralf Junghanns <ralf.junghanns@itexia.com>
2021-12-10 17:44:39 +0100

    Add landelino sensors

--- src-php/parseAmpeqData.sh
+++ src-php/parseAmpeqData.sh
@@ -11,3 +11,6 @@ docker-compose exec php bin/console app:parse-ampeq:bra2 PZ-04
 docker-compose exec php bin/console app:parse-ampeq:bra2 PZ-05
 docker-compose exec php bin/console app:parse-ampeq:bra2 PZ-06
 docker-compose exec php bin/console app:parse-ampeq:bra2 PZ-07
+docker-compose exec php bin/console app:parse-ampeq:bra2 EPA-11
+docker-compose exec php bin/console app:parse-ampeq:bra2 EPA-12
+docker-compose exec php bin/console app:parse-ampeq:bra2 EPA-13
