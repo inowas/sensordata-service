f3df51fcd6523f0b480b1364f3f9d097c86856c8
Ralf Junghanns <ralf.junghanns@itexia.com>
2021-12-13 11:13:12 +0100

    Update cron-script

--- src-php/parseAmpeqData.sh
+++ src-php/parseAmpeqData.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-docker-compose exec php bin/console app:parse-ampeq:bra2 PT-01
-docker-compose exec php bin/console app:parse-ampeq:bra2 PT-02
-docker-compose exec php bin/console app:parse-ampeq:bra2 PZ-01
-docker-compose exec php bin/console app:parse-ampeq:bra2 PZ-02
-docker-compose exec php bin/console app:parse-ampeq:bra2 PZ-03
-docker-compose exec php bin/console app:parse-ampeq:bra2 PZ-04
-docker-compose exec php bin/console app:parse-ampeq:bra2 PZ-05
-docker-compose exec php bin/console app:parse-ampeq:bra2 PZ-06
-docker-compose exec php bin/console app:parse-ampeq:bra2 PZ-07
-docker-compose exec php bin/console app:parse-ampeq:bra2 EPA-11
-docker-compose exec php bin/console app:parse-ampeq:bra2 EPA-12
-docker-compose exec php bin/console app:parse-ampeq:bra2 EPA-13
+docker-compose exec -T php bin/console app:parse-ampeq:bra2 PT-01
+docker-compose exec -T php bin/console app:parse-ampeq:bra2 PT-02
+docker-compose exec -T php bin/console app:parse-ampeq:bra2 PZ-01
+docker-compose exec -T php bin/console app:parse-ampeq:bra2 PZ-02
+docker-compose exec -T php bin/console app:parse-ampeq:bra2 PZ-03
+docker-compose exec -T php bin/console app:parse-ampeq:bra2 PZ-04
+docker-compose exec -T php bin/console app:parse-ampeq:bra2 PZ-05
+docker-compose exec -T php bin/console app:parse-ampeq:bra2 PZ-06
+docker-compose exec -T php bin/console app:parse-ampeq:bra2 PZ-07
+docker-compose exec -T php bin/console app:parse-ampeq:bra2 EPA-11
+docker-compose exec -T php bin/console app:parse-ampeq:bra2 EPA-12
+docker-compose exec -T php bin/console app:parse-ampeq:bra2 EPA-13
